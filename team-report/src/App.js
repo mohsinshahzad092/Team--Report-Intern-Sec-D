3eccd31185a85698792302e6af83c8a3ce92eeff
Mohsin Shahzad <72189089+mohsinshahzad092@users.noreply.github.com>
2021-11-29 14:21:16 +0500

    Login Form
    Login Form

--- team-report/src/App.js
+++ team-report/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Pages/Login/Login';
 
 function App() {
   console.log("hello")
   return (
     <div className="App">
+
+      <Login/>
       
     </div>
   );
---
+++ team-report/src/Pages/Login/Login.js
@@ -0,0 +1,91 @@
+import { Form, Input, Button, Checkbox, Typography } from 'antd';
+
+const { Title } = Typography;
+
+
+const Login = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+
+        <>
+            <div style={{ width: '100%', display:'flex' , justifyContent: 'center' }}>
+                <Title level={2}>Sign in</Title>
+
+            </div>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 4,
+                    offset: 4
+                }}
+                wrapperCol={{
+                    span: 8,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Login"
+                    name="Login"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="remember"
+                    valuePropName="checked"
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
+    );
+};
+
+
+export default Login;
